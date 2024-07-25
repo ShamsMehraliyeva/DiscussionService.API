da84da3288e0d9ee15d4449791c2099d04e36b9f
Shams Mehraliyeva <shams.mehralieva99@gmail.com>
2024-07-25 10:29:12 +0400

    fix: add migration mig_3

---
+++ src/Infrastructure/Persistence/Migrations/20240725060838_Mig_3.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Mig_3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedByIp",
+                table: "RefreshTokens");
+
+            migrationBuilder.RenameColumn(
+                name: "Expires",
+                table: "RefreshTokens",
+                newName: "ExpireDate");
+
+            migrationBuilder.RenameColumn(
+                name: "Created",
+                table: "RefreshTokens",
+                newName: "CreateDate");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Users",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Users");
+
+            migrationBuilder.RenameColumn(
+                name: "ExpireDate",
+                table: "RefreshTokens",
+                newName: "Expires");
+
+            migrationBuilder.RenameColumn(
+                name: "CreateDate",
+                table: "RefreshTokens",
+                newName: "Created");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedByIp",
+                table: "RefreshTokens",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
